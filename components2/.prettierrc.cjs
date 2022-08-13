7440c6f0c8bb112c2b88031817df2e2ce0139b99
taigawada <athtela2266@gmail.com>
2022-08-13 15:13:35 +0900

    vue-demiを用いてvue2とvue3のパッケージを統合

--- components2/.prettierrc.cjs
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    printWidth: 140,
-    tabWidth: 4,
-    semi: true,
-    singleQuote: true,
-};
--- components2/src/serve.ts
+++
@@ -1,9 +0,0 @@
-import Vue from 'vue';
-import { install } from 'vue-demi';
-install();
-
-import PlayGround from './PlayGround.vue';
-
-new Vue({
-    render: (h) => h(PlayGround),
-}).$mount('#app');
