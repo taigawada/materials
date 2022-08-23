f51ddde4921e852721fef148b258bc052bcd0aea
taigawada <athtela2266@gmail.com>
2022-08-23 23:24:00 +0900

    フォントとテキストカラーを各コンポーネントに設定

--- components/src/PlayGround.vue
+++ components/src/PlayGround.vue
@@ -493,11 +493,4 @@ export default defineComponent({
 body {
     background: #f4f4f4;
 }
-#app {
-    font-family: Avenir, Helvetica, Arial, sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    text-align: center;
-    color: #2c3e50;
-}
 </style>
