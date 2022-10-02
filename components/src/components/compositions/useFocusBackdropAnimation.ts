f89a60401a121b371ec7500f4e20b350251e8b07
TaigaWada <athtela2266@gmail.com>
2022-10-03 00:16:44 +0900

    add animation compositions

---
+++ components/src/components/compositions/useFocusBackdropAnimation.ts
@@ -0,0 +1,22 @@
+import { computed, ref } from 'vue-demi';
+import { useTransition, TransitionPresets } from '@vueuse/core';
+
+export const useFocusBackdrop = () => {
+    const source = ref(0);
+    const transitions = useTransition(source, {
+        duration: 140,
+        transition: TransitionPresets.easeInOutCubic,
+    });
+    const outputStyles = computed(() => `${transitions.value}px`);
+    const focusinFn = () => {
+        source.value = 1.5;
+    };
+    const focusoutFn = () => {
+        source.value = 0;
+    };
+    return {
+        outputStyles,
+        focusinFn,
+        focusoutFn,
+    };
+};
